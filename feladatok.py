9a55e60eaadbdc6e005c1ce19063f760bce29cff
Bence1102 <szente.bence04@gmail.com>
2024-10-17 11:21:35 +0200

    Többi

--- feladatok.py
+++ feladatok.py
@@ -5,7 +5,7 @@ Az alábbi metódusok paraméterként kapják a fenti listát.
 2.Mennyi a negatív számok összege?
 3.Mennyi az öttel osztható számok összege?
 '''
-
+import random
 
 
 def poz_szamok_szama(lista):
@@ -45,4 +45,66 @@ def ottel_oszthatoak(lista):
     return oszt
 
 
+
+'''Készíts függvényt, mely az érme dobás szimulálja és legenerál 10 db fej vagy írást
+és az eredmény eltárolja egy listában. A függvény térjen vissza a listával
+Továbbiakban ezzel a listával dolgozz az alábbi függvényekben: 
+számold meg hány fej dobás van a listában
+'''
+
+
+def ermedobas():
+    
+    eredmeny_lista=[]
+    i:int =0
+    while (i<10):
+        erem:int=int(random.random()*2+1)
+        if (erem==1):
+            eredmeny_lista.append("F")
+        elif (erem==2):
+            eredmeny_lista.append("Í")
+        i+=1
+    return eredmeny_lista
+
+def fej_dobas(eredmeny_lista):
+    i:int=0
+    fej:int=0
+    while (i<len(eredmeny_lista)):
+        if(eredmeny_lista[i]=="F"):
+            fej+=1
+        i+=1
+    return fej
+
+
+
+
+def kockadobas():
+    kocka_lista=[]
+    i:int =0
+    while (i<200):
+        kocka:int=int(random.random()*7)+1
+        if(kocka==1):
+            kocka_lista.append(1)
+        elif(kocka==2):                                                                                                                                                                                                                             
+            kocka_lista.append(2)
+        elif(kocka==3):                                                                                                                                                                                                                                
+            kocka_lista.append(3)
+        elif(kocka==4):                                                                                                                                                                                                                                  
+            kocka_lista.append(4)
+        elif(kocka==5):                                                                                                                                                                                                                                  
+            kocka_lista.append(5)
+        elif(kocka==6):
+            kocka_lista.append(6)
+        i+=1
+    return kocka_lista
+
+
+
+
+
+        
+
+        
+    
+            
     
--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import feladatok
 
-#lista=[12, 21, 56, 32, -5, -23, 35]
-'''print("1.feladat")
+lista=[12, 21, 56, 32, -5, -23, 35]
+print("1.feladat")
 db:int=feladatok.poz_szamok_szama(lista)
-print(f"A pozítiv számok száma {db}")'''
+print(f"A pozítiv számok száma {db}")
 
-'''lista=[12, 21, 56, 32, -5, -23, 35]
+lista=[12, 21, 56, 32, -5, -23, 35]
 print("2.feladat")
 vego:int=feladatok.negativ_szamok_osszege(lista)
-print(f"A negatív számok {vego}")'''
+print(f"A negatív számok {vego}")
 
 
 lista=[12, 21, 56, 32, -5, -23, 35]
 print("3.feladat")
 atlag:int=feladatok.ottel_oszthatoak(lista)
-print(f"5-tel osztható számok átlaga {atlag}")
+print(f"5-tel osztható számok átlaga: {atlag}")
+
+
+
+print("4.feladat")
+eredmeny_lista=feladatok.ermedobas()
+print(f"Érme dobás: {eredmeny_lista}")
+
+ 
+print("4.feladat 2.része")
+Fej=feladatok.fej_dobas(eredmeny_lista)
+print(f"Mennyi fejet dobtunk: {Fej}")
+
+
+print("5.feladat")
+dobo_kocka=feladatok.kockadobas(eredmeny_lista)
+print(f"Dobások: {dobo_kocka}")
